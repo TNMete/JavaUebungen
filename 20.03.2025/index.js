84b75812d391a5f0675502af383aa7104fac115f
TNMete <mete.karaoglan@tn.techstarter.de>
2025-03-20 22:30:37 +0100

    update

--- 20.03.2025/index.js
+++ 20.03.2025/index.js
@@ -1,15 +1,53 @@
-let name = prompt("Wie heißt du, tapferer Held?");
-alert("Freut mich, dich kennenzulernen, " + name + "!");
+// let vname = prompt("Hallo, wie lautet dein Name?");
+// alert("wow, " + vname + " ist aber ein schöner Name!")
 
-let alter = prompt("Wie alt bist du?");
-if (alter >= 18) {
-    alert("Du bist alt genug, um in den Kampf zu ziehen!");
-    let waffe = prompt("Wählst du das Schwert oder Magie?");
-    if (waffe.toLowerCase() === "schwert") {
-        alert("Du schwingst dein Schwert und kämpfst tapfer!");
-    } else {
-        alert("Du wirkst einen mächtigen Zauber!");
-    }
-} else {
-    alert("Du bist noch zu jung. Trainiere weiter und kehre später zurück!");
+// let age = prompt("Wie alt bist du?");
+// age = Number(age);
+
+// if (age < 18) {
+//     alert("Du bist noch zu jung!")
+// } else {
+//     alert("Herzlich willkommen auf unserer Sete!")
+// }
+
+// let number = prompt("Gib eine Zahl ein: ")
+// number = Number(number);
+
+// if (number > 0) {
+//     alert("Die Zahl ist positiv!")
+// } else if (number < 0) {
+//     alert("Die Zahl ist negativ!")
+// } else {
+//     alert("Die zahl ist exakt null!")
+// }
+
+let vname = prompt("Hey, wie heißt du?");
+let nname = prompt("Gib bitte auch deinen Nachnamen ein!");
+alert("Oh wow, " + vname + " du hast aber einen schönen Nachnamen, das hörst du sicher oft oder? ;)")
+
+let dreamDestionation = prompt("Bitte nenne uns dein Lieblingsreise-Ziel.");
+if (dreamDestionation === "Italien" || dreamDestionation === "Toskana") {
+    alert("Hast du dort nicht deinen Antrag bekommen???");
+} if (dreamDestionation === "Türkei") {
+    alert("Da kommt doch dein Verlobter her? :o");
 }
+else {
+    alert(dreamDestionation + " ist eine sehr schöne Wahl!")
+}
+
+let firstQDestination = prompt("Wieso hast du dich für diesen Ort entschieden?");
+alert("Das ist eine niedliche Antowort <3")
+
+let secondQDestination = prompt("Was ist dein Lieblingsessen? \n1. Pizza\n2. Kartoffeln mit Rosmarin deiner Mama\n3. Türkische Pizza meiner Schwiegermutter\n4. trockenes Brot ;)")
+if (secondQDestination === "1." || secondQDestination === "1") {
+    alert("langweilig! (es sei denn ein Queere Frau aus Neapel hat sie gebracht :D)");
+} if (secondQDestination === "2." || secondQDestination === "2") {
+    alert("Lecker! Bald kannst du es ja wieder genießen! :D")
+} if (secondQDestination === "3." || secondQDestination === "3") {
+    alert("Du kleiner Schleimer! :D")
+} if (secondQDestination === "4." || secondQDestination === "4") {
+    alert("*kotz*")
+}
+
+let funFrage = prompt('Morgen kocht dein "Freund" für dich, was wünschst du dir?');
+alert("HAHAHAHA als ob, morgen gehen wir Fischies holen, steh jetzt auf vom PC, meine Hausaufgaben sind fertig <3\n Ich liebe dich.")
