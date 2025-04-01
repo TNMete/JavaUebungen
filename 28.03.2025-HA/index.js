5c2a9e3913a29229e3565be03986784e23102ccd
TNMete <mete.karaoglan@tn.techstarter.de>
2025-04-01 14:48:44 +0200

    up

--- 28.03.2025-HA/index.js
+++ 28.03.2025-HA/index.js
@@ -3,9 +3,7 @@ const cors = require("cors")
 const app = express();
 const fs = require("fs");
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5500'
-}));
+app.use(cors({ origin: ["http://localhost:5500", "http://127.0.0.1:5500"] }));
 
 function readFile() {
     const data = fs.readFileSync("carlist.json", "utf-8");
