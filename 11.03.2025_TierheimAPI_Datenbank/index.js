3e9781f1c71bdcf60e31df29e7c1cbfac13ffa77
TNMete <mete.karaoglan@tn.techstarter.de>
2025-04-11 21:59:30 +0200

    up

---
+++ 11.03.2025_TierheimAPI_Datenbank/index.js
@@ -0,0 +1,88 @@
+const express = require("express")
+const app = express()
+const sqlite3 = require("sqlite3")
+
+const db = new sqlite3.Database(":memory:")
+
+db.serialize(() => {
+    db.run(`CREATE TABLE tiere (
+    id INTEGER PRIMARY KEY,
+    tierart VARCHAR(50),
+    name VARCHAR(50),
+    krankheit VARCHAR(100),
+    age INT,
+    gewicht REAL);`)
+
+    selectAllTiereQuery = `SELECT * FROM tiere`
+    db.all(selectAllTiereQuery, (err, rows) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(rows)
+        }
+    })
+    process.on("exit", () => {
+        db.close()
+    })
+})
+
+app.use(express.json()) // Ermöglicht Express Json aus einem Body auszulesen
+app.get("/", (req, res) => {
+    res.send("Die API funktioniert!")
+})
+
+app.get("/tiere", (req, res) => {
+    db.all(selectAllTiereQuery, (err, rows) => {
+        if (err) {
+            res.status(404).send("Fehler in deiner Query Anfrage")
+        } else {
+            res.json(rows)
+        }
+    })
+})
+
+app.get("/tiere/:id", (req, res) => {
+    const id = req.params.id
+    db.get(`SELECT * FROM tiere WHERE id = ?`, [id], (err, row) => {
+        if (err) {
+            res.status(500).send("Fehler beim Abrufen des Tieres")
+        } else if (!row) {
+            res.status(404).send("Tier nicht gefunden")
+        } else {
+            res.json(row)
+        }
+    })
+})
+
+app.put("/tiere/:id", (req, res) => {
+    const id = req.params.id
+    const { tierart, name, krankheit, age, gewicht } = req.body
+    db.run(
+        `UPDATE tiere SET tierart = ?, name = ?, krankheit = ?, age = ?, gewicht = ? WHERE id = ?`,
+        [tierart, name, krankheit, age, gewicht, id],
+        function (err) {
+            if (err) {
+                res.status(500).send("Fehler beim Aktualisieren")
+            } else if (this.changes === 0) {
+                res.status(404).send("Tier nicht gefunden")
+            } else {
+                res.send("Tier erfolgreich aktualisiert")
+            }
+        }
+    )
+})
+
+app.delete("/tiere/:id", (req, res) => {
+    const id = req.params.id
+    db.run(`DELETE FROM tiere WHERE id = ?`, [id], function (err) {
+        res.send("Tier erfolgreich gelöscht")
+    })
+})
+
+app.post("/tiere", (req, res) => {
+    const { tierart, name, krankheit, age, gewicht } = req.body
+    db.run(`INSERT INTO tiere (tierart,name,krankheit,age,gewicht) VALUES(?,?,?,?,?)`, [tierart, name, krankheit, age, gewicht])
+    res.status(201).send("Tier wurde erfolgreich hinzugefügt")
+})
+
+app.listen(5050)
